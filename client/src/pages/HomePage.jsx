15019e66043a760c6f758d43628f9625f092c86e
Gerard-rmo <gerardromeroglaive@proton.me>
2025-06-19 13:00:40 +0200

    fix delpoy 6

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,3 @@
-
 import logo from '@assets/logo.webp';
 import slide1 from '@assets/T1.jpg';
 import slide2 from '@assets/T2.jpg';
@@ -13,7 +12,20 @@ import slide10 from '@assets/couv.png';
 import slide11 from '@assets/az.png';
 import "./HomePage.css";
 
-const slides = [slide1, slide2, slide3, slide4, slide5, slide6,slide7, slide8, slide9, slide10, slide11]; 
+// ✅ Key/value format (with descriptive alt texts)
+const slides = [
+  { src: slide1, alt: "Tome 1" },
+  { src: slide2, alt: "Tome 2" },
+  { src: slide3, alt: "Tome 3 couverture B" },
+  { src: slide4, alt: "Tome 3 couverture M" },
+  { src: slide5, alt: "Akelarre" },
+  { src: slide6, alt: "Alem" },
+  { src: slide7, alt: "Mosquito" },
+  { src: slide8, alt: "Les Protecteurs" },
+  { src: slide9, alt: "Couverture alternative 2" },
+  { src: slide10, alt: "Couverture alternative 1" },
+  { src: slide11, alt: "AZ illustration" },
+];
 
 const HomePage = () => {
   return (
@@ -28,9 +40,9 @@ const HomePage = () => {
 
       <div className="carousel-frise">
         <div className="carousel-track-frise">
-          {[...slides, ...slides].map((img, index) => (
+          {[...slides, ...slides].map((slide, index) => (
             <div className="carousel-item-frise" key={index}>
-              <img src={img} alt={`Slide ${index + 1}`} />
+              <img src={slide.src} alt={slide.alt} />
             </div>
           ))}
         </div>
@@ -40,3 +52,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
